9c456d4ce241e089c4b135821a6e481b1586198b
Artur Signell <artur@vaadin.com>
2021-12-10 13:06:28 +0200

    test

--- src/my-element.ts
+++ src/my-element.ts
@@ -1,56 +1,25 @@
-import { html, css, LitElement } from 'lit'
-import { customElement, property } from 'lit/decorators.js'
+import '@vaadin/select';
+import { html, LitElement, render } from 'lit';
+import { unsafeHTML } from 'lit/directives/unsafe-html.js';
+import { customElement } from 'lit/decorators.js';
 
-/**
- * An example element.
- *
- * @slot - This element has a slot
- * @csspart button - The button
- */
 @customElement('my-element')
 export class MyElement extends LitElement {
-  static styles = css`
-    :host {
-      display: block;
-      border: solid 1px gray;
-      padding: 16px;
-      max-width: 800px;
-    }
-  `
-
-  /**
-   * The name to say "Hello" to.
-   */
-  @property()
-  name = 'World'
-
-  /**
-   * The number of times the button has been clicked.
-   */
-  @property({ type: Number })
-  count = 0
 
   render() {
     return html`
-      <h1>Hello, ${this.name}!</h1>
-      <button @click=${this._onClick} part="button">
-        Click Count: ${this.count}
-      </button>
-      <slot></slot>
+      <vaadin-select .renderer=${this.renderSelect} label="Who are you"></vaadin-select>
     `
   }
 
-  private _onClick() {
-    this.count++
-  }
-
-  foo(): string {
-    return 'foo'
-  }
-}
-
-declare global {
-  interface HTMLElementTagNameMap {
-    'my-element': MyElement
+  private renderSelect(root: HTMLElement) {
+    const listbox = ` 
+    <vaadin-list-box>
+      <vaadin-item value="me">me</vaadin-item>
+      <vaadin-item value="you">you</vaadin-item>
+    </vaadin-list-box>
+    `;
+    root.innerHTML= listbox;
+    //render(unsafeHTML(listbox), root); // This works because coincidentally Lit render is a no-op
   }
 }
